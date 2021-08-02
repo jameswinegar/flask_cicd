476631fe44b3e1206691593decd64860e4f50aea
James Winegar <jameswine@gmail.com>
2021-08-02 04:48:56 -0500

    add simple test

---
+++ tests/test_root.py
@@ -0,0 +1,25 @@
+import unittest
+from unittest import TestCase
+
+from api.app import create_app
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        self.app = create_app("testing")
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        self.client = self.app.test_client(use_cookies=True)
+
+    def tearDown(self):
+        """
+        Will be called after every test
+        """
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_root_response(self):
+        response = self.client.post("/", headers={"Content-Type": "application/json"},)
+
+        self.assertEqual(response.status_code, 200)
