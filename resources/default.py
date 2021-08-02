3d289c6c1a3d15b345c54b1c53024005ec31889f
James Winegar <jameswine@gmail.com>
2021-08-02 04:39:03 -0500

    hello world api

---
+++ resources/default.py
@@ -0,0 +1,14 @@
+from flask_restful import Resource
+
+from api.app import api
+
+
+class DefaultResource(Resource):
+    """Handle default route."""
+
+    def get(self):
+        """Get request for home page or response."""
+        return {"status": "success", "data": {"msg": "Welcome to our API"}}
+
+
+api.add_resource(DefaultResource, "/", endpoint="home")
